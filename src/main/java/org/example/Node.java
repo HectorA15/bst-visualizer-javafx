31d0a70c8d1a0d1b6908f48727bd9bcb043209eb
Angel Ramiro Solis Beltran <204301828+Angelsol2@users.noreply.github.com>
2025-11-19 14:34:21 -0600

    Add previous node reference to Node class
    Refactored Node class to include previous node reference and updated constructor parameter name.

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -3,15 +3,16 @@ package org.example;
 import javafx.scene.control.Button;
 
 public class Node {
+    public int weight;
+    public Node left, right;
+    public Node previous;
     public Button visual;
-    private int weight;
-    private Node left;
-    private Node right;
-    Node visual;
 
-
-    public Node(int weight) {
-        this.weight = weight;
+    public Node(int peso ) {
+        this.weight = peso;
+        this.right = null;
+        this.left = null;
+        this.previous = null;
     }
 
     public int getWeight() {
@@ -37,4 +38,19 @@ public class Node {
     public void setRight(Node right) {
         this.right = right;
     }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+
+
 }
+
+
+
+
